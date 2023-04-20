39bff8ef70aaf321f4dee516240cbbf7eba38c04
Fer Illanes <illanes.f.m@gmail.com>
2023-04-20 15:48:45 -0300

    primera version

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const button1 = document.querySelector('.btn-rotate-1');
+const button2 = document.querySelector('.btn-rotate-2');
+const button3 = document.querySelector('.btn-rotate-3');
+
+button1.addEventListener('click', function() {
+  this.classList.toggle('rotate');
+});
+
+button2.addEventListener('click', function() {
+  this.classList.toggle('rotate');
+});
+
+button3.addEventListener('click', function() {
+  this.classList.toggle('rotate');
+});
